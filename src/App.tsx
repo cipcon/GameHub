1ad2396e85fcea2dcd2ed669ccac7444e734e699
Constantin Ciprian <ciprianconstantin@protonmail.com>
2025-03-05 19:44:36 +0100

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const App = () => {
-  return <div>App</div>;
+  return <Button colorScheme="blue">Button</Button>;
 };
 
 export default App;
