a238d0ed676b72c6504ff7abb7161635e8cf2f74
Constantin Ciprian <ciprianconstantin@protonmail.com>
2025-03-31 21:13:22 +0200

    changed the base property to the project name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GameHub/',
   server: {
     hmr: {
       overlay: false, // Dezactivează reîncărcarea automată la erori
